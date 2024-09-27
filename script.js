84a86d2307514aa263b3fd3dc208a5fb3b1b0ec9
Maharufh Ali Mir <85804873+maharufh@users.noreply.github.com>
2024-09-27 11:34:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example JavaScript to toggle "View More" button behavior
+document.querySelector('.view-more-btn').addEventListener('click', function () {
+    alert('More courses will be available soon!');
+  });
+  
